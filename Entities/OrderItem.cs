12f1124e2d4d3d555d03241d3961d390f349fb89
Danilo Fontes <danilo_f_de-carvalho@estudante.sc.senai.br>
2020-02-18 03:02:21 -0300

    ola

---
+++ Entities/OrderItem.cs
@@ -0,0 +1,38 @@
+﻿using System.Globalization;
+
+namespace Composition3.Entities
+{
+    class OrderItem
+    {
+        public int Quantity { get; set; }
+        public double Price { get; set; }
+        public Product Product { get; set; }
+
+        public OrderItem()
+        {
+        }
+
+        public OrderItem(Product product, int quantity, double price)
+        {
+            Product = product;
+            Quantity = quantity;
+            Price = price;
+        }
+
+        public double subTotal()
+        {
+            return Quantity * Price;
+        }
+
+        public override string ToString()
+        {
+            return Product.Name
+                + ", $"
+                + Price.ToString("f2", CultureInfo.InvariantCulture)
+                + ", Quantity: "
+                + Quantity
+                + ", Subtotal: $"
+                + subTotal().ToString("f2", CultureInfo.InvariantCulture);
+        }
+    }
+}
